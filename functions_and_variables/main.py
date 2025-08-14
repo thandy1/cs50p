42c5048d4639f1c13dd70a7d59183cc4ccddedb2
Terren Handy <213094058+thandy1@users.noreply.github.com>
2025-08-14 13:03:05 -0500

    Initial commit

---
+++ functions_and_variables/main.py
@@ -0,0 +1,12 @@
+# Ask user for their name
+name = input("Whats your name? ").strip().title()
+
+# Split user's name into first name and last name
+first, last = name.split(" ")
+
+# Say hello to user
+print(f"Hello, {first}")
+
+
+
+
